66017181ae6b398916a150e9d5fe131d7c778d80
helenadouglas <helena.m.douglas02@gmail.com>
2026-01-06 15:25:10 +0100

    Refactor contact page layout and update favicon for light/dark mode

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,10 @@ import ModeToggle from "./components/ModeToggle";
 
 export const metadata = {
     title: "Helena Douglas",
+    icons: [
+        { rel: "icon", url: "/favicon_lightmode.png", media: "(prefers-color-scheme: light)" },
+        { rel: "icon", url: "/favicon_darkmode.png", media: "(prefers-color-scheme: dark)" },
+    ],
 };
 
 export default function RootLayout({ children }) {
